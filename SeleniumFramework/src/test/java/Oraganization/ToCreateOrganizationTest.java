a051bc13a1e8ed96739ad51d57a9843ce2ce6e84
irinr <irinr@IRINRAGHUL>
2024-10-04 17:34:16 +0530

    Edited copy of Sathyapriya

---
+++ SeleniumFramework/src/test/java/Oraganization/ToCreateOrganizationTest.java
@@ -0,0 +1,8 @@
+package Oraganization;
+
+public class ToCreateOrganizationTest {
+	public static void main(String[] args) {
+		System.out.println("organization created successfully by Raghul");
+	}
+
+}
---
+++ SeleniumFramework/src/test/java/leads/ToCreateLeadsTest.java
@@ -0,0 +1,9 @@
+package leads;
+
+public class ToCreateLeadsTest {
+
+	public static void main(String[] args) {
+		System.out.println("Contact created Successfully by Raghul");
+	}
+
+}
